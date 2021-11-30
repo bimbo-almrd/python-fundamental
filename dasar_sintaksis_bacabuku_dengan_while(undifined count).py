2e879af8adad7a1870f0283a3f52d8932aa4ebc0
bimbo almarado <bimboalmrd@gmail.com>
2021-11-30 13:41:43 +0700

    singkatkan variabel

--- dasar_sintaksis_bacabuku_dengan_while(undifined count).py
+++ dasar_sintaksis_bacabuku_dengan_while(undifined count).py
@@ -3,22 +3,23 @@ perulangan dengan while sampai paham
 """
 jumlah_buku = 10
 print('ibu berkata, baca semua bukumu sampai paham')
-total_jumlah_baca = 0
-jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+jumlah_baca = 0
 
-while total_jumlah_baca < jumlah_buku * 2:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
-        print(f"buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum di pahami")
+jumlah_baca_paham = 0
+print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_baca_paham}')
+
+while jumlah_baca < jumlah_buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_baca_paham == 9:
+        print(f"buku ke {jumlah_baca_paham + 1} belum di pahami")
     else:
-        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f'buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} yang sudah dibaca dan dpahami')
+        jumlah_baca_paham = jumlah_baca_paham + 1
+        print(f'buku ke {jumlah_baca_paham} yang sudah dibaca dan dpahami')
 
 
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku ')
-if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
-    print (f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_baca_paham} buku ')
+if jumlah_baca_paham == jumlah_buku:
+    print (f'jumlah buku yang sudah dibaca dan dipahami {jumlah_baca_paham}')
 else:
     print(f'"Bu, tidak semua buku bisa dipahami, '
-          f'Budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+          f'Budi hanya bisa memahami {jumlah_baca_paham}')
